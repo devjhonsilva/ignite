e1a8338c735dd0e3418d8b6452d56abb102a6c7f
Jonatan Santos <jonatansilvasantos@outlook.com>
2024-05-19 05:33:09 -0300

    Criando Projeto Ignite

---
+++ src/screens/Groups/index.tsx
@@ -0,0 +1,10 @@
+import {Container, Text} from './styles';
+
+export function Groups() {
+  return (
+    <Container>
+      <Text>Groups</Text>
+
+    </Container>
+  );
+}
---
+++ src/screens/Groups/styles.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+    background-color: #000;
+    flex: 1;
+    align-items: center;
+    justify-content: center;
+`;
+export const Text = styled.Text`
+    color: #fff;
+    font-size: 40px;
+`
